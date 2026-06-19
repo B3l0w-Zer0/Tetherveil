6fe622259442bc3bbc505effbc8a479c2a785f7d
Jan Hannig <janhannig210705@gmail.com>
2026-06-19 18:52:49 +0200

    advancements in monster creation and start of fight scene (first method for creating own player field)

--- src/scenes/fight.js
+++ src/scenes/fight.js
@@ -76,6 +76,13 @@ export class Fight extends Phaser.Scene {
 
 function createOwnField(){
   const ownFieldWrapper = document.createElement('div')
+  ownFieldWrapper.style.position = "absolute";
+  ownFieldWrapper.style.left = "2%";
+  ownFieldWrapper.style.bottom = "5%";
+  ownFieldWrapper.style.height = "25%";
+  ownFieldWrapper.style.width = "40%";
+  ownFieldWrapper.style.backgroundColor = "#FF6A2A";
+  ownFieldWrapper.innerText = "hier ist deine monster card";
 
   const ownMonstSprite = document.createElement('div');
   ownMonstSprite.style.position = "absolute";
@@ -87,36 +94,126 @@ function createOwnField(){
   ownMonstSprite.innerText = "hier ist dein monster sprite";
 
 // field with all own monster parts
-  const ownMonstWrapper = document.createElement('div');
+  /*const ownMonstWrapper = document.createElement('div');
   ownMonstWrapper.style.position = "absolute";
-  ownMonstWrapper.style.left = "3%";
-  ownMonstWrapper.style.bottom = "6%";
-  ownMonstWrapper.style.height = "15%";
-  ownMonstWrapper.style.width = "25%";
+  ownMonstWrapper.style.left = "2%";
+  ownMonstWrapper.style.bottom = "5%";
+  ownMonstWrapper.style.height = "25%";
+  ownMonstWrapper.style.width = "40%";
   ownMonstWrapper.style.backgroundColor = "#FF6A2A";
   ownMonstWrapper.innerText = "hier ist deine monster card";
 
+   */
+
+  const basicInfoField = document.createElement('div')
+  basicInfoField.classList.add('basic-info-field')
+
+  const nameInfoField = document.createElement('div')
+  nameInfoField.classList.add('single-info-field-name')
+
+  const levelInfoField = document.createElement('div')
+  levelInfoField.classList.add('single-info-field-level')
+
+  const typeInfoField = document.createElement('div')
+  typeInfoField.classList.add('single-info-field-type')
+
+  basicInfoField.appendChild(nameInfoField)
+  basicInfoField.appendChild(levelInfoField)//hier noch eine leiste einfügen wo die EP auch grafisch dargestellt werden
+  basicInfoField.appendChild(typeInfoField)
+
+  const extendedInfoBtn = document.createElement('btn')
+  extendedInfoBtn.classList.add('extended-info-button')
+
+  const healthBar = document.createElement('div');
+  healthBar.classList.add('health-bar-fight')
+
+  const manaBar = document.createElement('div');
+  manaBar.classList.add('mana-bar-fight')
+
+  const staminaBar = document.createElement('div');
+  staminaBar.classList.add('stamina-bar-fight')
+
   const openActionsFieldBtn = document.createElement('btn')
+  openActionsFieldBtn.classList.add("action-button")
+
+  const openAttackFieldBtn = document.createElement('btn')
+  openAttackFieldBtn.classList.add("attack-button")
+
+  const openRunAwayFieldBtn = document.createElement('btn')
+  openRunAwayFieldBtn.classList.add("run-away-button")
+
+  ownFieldWrapper.appendChild(basicInfoField)
+  ownFieldWrapper.appendChild(extendedInfoBtn)
+  ownFieldWrapper.appendChild(healthBar)
+  ownFieldWrapper.appendChild(manaBar)
+  ownFieldWrapper.appendChild(staminaBar)
+  ownFieldWrapper.appendChild(openAttackFieldBtn)
+  ownFieldWrapper.appendChild(openRunAwayFieldBtn)
+  ownFieldWrapper.appendChild(openActionsFieldBtn)
 
 // wrapper/field for your monsters attacks
-  const ownMonstActionsWrapper = document.createElement('div');
+ /* const ownMonstActionsWrapper = document.createElement('div');
   ownMonstActionsWrapper.style.position = "absolute";
   ownMonstActionsWrapper.style.display = "grid";
-  ownMonstActionsWrapper.style.gridTemplateColumns = "1fr 1fr";
-  ownMonstActionsWrapper.style.gap = "10px";
+  ownMonstActionsWrapper.style.gridTemplateColumns = 'repeat(3, 1fr)';
+  ownMonstActionsWrapper.style.gap = "2%x";
   ownMonstActionsWrapper.style.left = "1%";
   ownMonstActionsWrapper.style.bottom = "2%";
   ownMonstActionsWrapper.style.height = "70%";
   ownMonstActionsWrapper.style.width = "60%";
-  ownMonstActionsWrapper.style.backgroundColor = "#A6FF2A";
-  ownMonstActionsWrapper.innerText = "Hier sind deine Attacken";
+  ownMonstActionsWrapper.style.backgroundColor = "purple";
+  //ownMonstActionsWrapper.innerText = "Hier sind deine aktuellen Möglichkeiten. Catch, run away, switch, actions";
 
-  ownFieldWrapper.appendChild(ownMonstSprite);
-  ownMonstWrapper.appendChild(ownMonstActionsWrapper);
-  ownFieldWrapper.appendChild(ownMonstWrapper);
+
+  */
+  /*
+  const buttonGroup = document.createElement('div');
+  buttonGroup.style.position = "absolute";
+  buttonGroup.style.display = "grid";
+  buttonGroup.style.gridTemplateColumns = 'repeat(3, 1fr)';
+  buttonGroup.style.gap = "2%x";
+  buttonGroup.style.left = "1%";
+  buttonGroup.style.bottom = "2%";
+  buttonGroup.style.height = "70%";
+  buttonGroup.style.width = "60%";
+  buttonGroup.style.backgroundColor = "purple";
+  //buttonGroup.innerText = "Hier sind deine aktuellen Möglichkeiten. Catch, run away, switch, actions";
+
+
+
+
+  /*const button1 = document.createElement('div')
+  button1.style.position = "relative"
+  button1.style.height = "80%";
+  button1.style.width = "70%";
+  button1.style.backgroundColor = "blue";
+  button1.innerText = "Hier is möglichkeit 1";
+
+  const button2 = document.createElement('div')
+  button2.style.position = "relative"
+  button2.style.height = "80%";
+  button2.style.width = "70";
+  button2.style.backgroundColor = "red";
+  button2.innerText = "Hier is möglichkeit 2";
+
+  const button3 = document.createElement('div')
+  button3.style.position = "relative"
+  button3.style.height = "80%";
+  button3.style.width = "70%";
+  button3.style.backgroundColor = "yellow";
+  button3.innerText = "Hier is möglichkeit 3";
+
+  ownMonstActionsWrapper.appendChild(button1)
+  ownMonstActionsWrapper.appendChild(button2)
+  ownMonstActionsWrapper.appendChild(button3)*/
+
+  //ownMonstActionsWrapper.appendChild(ownMonstActionsWrapper)
+  //ownFieldWrapper.appendChild(ownMonstSprite);
+  //ownMonstWrapper.appendChild(ownMonstActionsWrapper);
+  //ownFieldWrapper.appendChild(ownMonstWrapper);
 
   //field for own player
-  const ownPlayerField = document.createElement('div');
+  /*const ownPlayerField = document.createElement('div');
   ownPlayerField.style.position = "absolute";
   ownPlayerField.style.right = "35%";
   ownPlayerField.style.bottom = "4%";
@@ -125,14 +222,19 @@ function createOwnField(){
   ownPlayerField.style.backgroundColor = "#8D479E"
   ownPlayerField.innerText = "Hier ist dein player Feld";
 
-  ownFieldWrapper.appendChild(ownPlayerField);
+   */
+
+  //ownFieldWrapper.appendChild(ownPlayerField);
+
+
 
   return ownFieldWrapper;
 }
 
 function createActionsMenu(){
   const actionsMenu = document.createElement('div')
-  actionsMenu.style.b
+  actionsMenu.style.bottom = "10%";
+
 
 
   return actionsMenu;
@@ -150,6 +252,34 @@ function switchAttackField(){
 
 }
 
+function createEnemyField(){
+  const enemyFieldWrapper = document.createElement('div');
+  enemyFieldWrapper.classList.add("enemy-field-wrapper")
+
+  const enemyInfoField = document.createElement('div');
+  enemyInfoField.classList.add("")
+
+  const enemyName = document.createElement('div')
+  enemyName.classList.add("")
+
+  const enemyLevel = document.createElement('div')
+  enemyLevel.classList.add("")
+
+  const enemyHealthBar = document.createElement('div');
+  enemyHealthBar.classList.add("")
+
+
+  return enemyFieldWrapper
+}
+
 //todo: for later when we finally get to the multi fight with several monsters out at the same time, find a solution to do this again but with a multiple option
-//like when starting ask for single or multi fight and then just do a switch case or else or shi like that to either call the createPlayerMenu once at that space where it is right now
-//or at like the spaces where it is supposed to be in each constellation of 2, 3, 4, 5 monsters out at the same time
+// like when starting ask for single or multi fight and then just do a switch case or else or shi like that to either call the createPlayerMenu once at that space where it is right now
+// or at like the spaces where it is supposed to be in each constellation of 2, 3, 4, 5 monsters out at the same time
+
+//todo: spätere funktion für Kampf: scan: Analyse des Gegners durch Fähigkeit mit Typ, Attacken, stats. je nach upgrade grad (gibt 4 level dazu)
+//      - 1. stufe: typ (und second type im hintergrund), strengthlevel durch base stats durchschnitt (das gibts als basic erste funktion im skill tree
+//      um zu erzwingen, dass der player das hat. ohne kann er nicht spielen und ohne das scannen wird das monster auch nicht in das grimoire vollständig aufgenommen.
+//      zwang, um jedes mal scan einzusetzen. (wenn allerdings monster gefangen, wird der vollständige eintrag automatisch gemacht oder man muss was anderes machen um eben zu analysieren/ zu erforschen
+//      - 2. stufe: genaue base stats
+//      - 3. stufe: Fähigkei
+//      - 4. stufe: attacken und
